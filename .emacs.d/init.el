25ba252a48f05077fa081fd26989d686c02378c9
Andy Grover <agrover@redhat.com>
2014-09-29 13:44:29 -0700

    put quotes around git grep's search
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -116,7 +116,7 @@
 (defun git-grep (search)
   "git-grep the entire current repo"
   (interactive (list (completing-read "Search for: " nil nil nil (current-word))))
-  (grep-find (concat "git --no-pager grep --no-color -n " search " `git rev-parse --show-toplevel`")))
+  (grep-find (concat "git --no-pager grep --no-color -n \"" search "\" `git rev-parse --show-toplevel`")))
 
 (global-set-key "\M-s" 'git-grep)
 
