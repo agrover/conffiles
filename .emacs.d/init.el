3e6cfb2489be7940acd3fdf827db9a3079756451
Andy Grover <agrover@redhat.com>
2017-11-20 10:25:46 -0800

    Make question marks red in Rust

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,7 +42,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "PfEd" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "PfEd" :family "Inconsolata"))))
+ '(rust-question-mark-face ((t (:inherit font-lock-builtin-face :foreground "orange red" :weight bold)))))
 
 (setq load-path (cons "~/.emacs.d/lisp" load-path))
 (setq grep-highlight-matches t)
