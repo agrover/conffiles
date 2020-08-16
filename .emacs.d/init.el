ffe024c3d9b88fbcfc6fe8656add6fac14dfccd5
Andy Grover <andy@groveronline.com>
2020-08-16 12:25:45 -0700

    Use Droid Sans

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,7 +51,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 143 :width normal :foundry "1ASC" :family "Droid Sans Mono"))))
  '(cycbuf-current-face ((t (:background "gray41" :weight bold))))
  '(cycbuf-header-face ((t (:foreground "DodgerBlue1" :weight bold))))
  '(rust-question-mark-face ((t (:inherit font-lock-builtin-face :foreground "orange red" :weight bold)))))
